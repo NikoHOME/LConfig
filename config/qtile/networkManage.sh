ada48332b87766fa39b151f7ea661065334cdfc1
Nikos <wiktor.gawronski@gmail.com>
2022-06-23 21:35:26 +0200

    Update networkManage.sh

--- config/qtile/networkManage.sh
+++ config/qtile/networkManage.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 
-DIALOG_RESULT=$(nmcli device status | grep PLAY);
+BEST_RESULT="$(nmcli --fields SSID device wifi list |head -2 | tail -1)";
+CURRENT="$(nmcli --fields CONNECTION device status | head -2 | tail -1)";
+BEST_RESULT="$(echo -e " \t$BEST_RESULT\t " | sed 's/^[ \t]//;s/[ \t]$//')";
+CURRENT="$(echo -e " \t$CURRENT\t " | sed 's/^[ \t]//;s/[ \t]$//')";
+echo $BEST_RESULT;
+echo $CURRENT;
+DIALOG_RESULT=$(nmcli --fields STATE device status | grep -w connected);
+echo $DIALOG_RESULT;
 if [ "$DIALOG_RESULT" = "" ];
 then
-    exec nmcli con up PLAY
+    exec nmcli con up "$BEST_RESULT" 
 else
-    exec nmcli con down PLAY  
+    exec nmcli con down "$CURRENT" 
 fi
