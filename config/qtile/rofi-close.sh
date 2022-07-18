9c580844e5e7661f92adb5b69f86c76b121cefb0
NikoHOME <wiktor.gawronski@gmail.com>
2022-07-18 15:37:18 +0200

    Update Configs

--- config/qtile/rofi-close.sh
+++ config/qtile/rofi-close.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-DIALOG_RESULT=$(echo -e 'cancel\nexit qtile\nreboot\nshutdown' | rofi -dmenu -i -p "[ Computer ] : " -hide-scrollbar -tokenize -lines 4 -eh 1 -width 40 -location 8 -xoffset 170 -yoffset 70 -padding 30 -disable-history -font "JetBrainsMono Nerd Font 18")
+DIALOG_RESULT=$(echo -e 'cancel\nexit qtile\nreboot\nshutdown\nhibernate\nsleep' | rofi -dmenu -i -p "   Computer  " -hide-scrollbar -tokenize -lines 4 -eh 1 -width 40 -location 8 -xoffset 170 -yoffset 70 -padding 30 -disable-history -font "JetBrainsMono Nerd Font 18")
 
 #echo "This result is : $DIALOG_RESULT"
 sleep 0.5;
@@ -14,4 +14,10 @@ then
 elif [ "$DIALOG_RESULT" = "shutdown" ];
 then
     exec shutdown -h now
+elif [ "$DIALOG_RESULT" = "hibernate" ];
+then
+    exec systemctl hibernate
+elif [ "$DIALOG_RESULT" = "sleep" ];
+then
+    exec systemctl suspend
 fi
