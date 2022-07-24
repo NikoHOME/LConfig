4f1b4bfdcd597883265c5105bf8373360a01bb49
NikoHOME <wiktor.gawronski@gmail.com>
2022-07-24 20:49:44 +0200

    Update Basic Install

--- install-extra.sh
+++ install-extra.sh
@@ -1,24 +1,12 @@
-pacman -Sy firefox thunar ranger git neofetch picom neovim lightdm-webkit2-greeter fish dmenu rofi python python-psutil python-iwlib alsa-utils ttf-ubuntu-font-family ttf-font-awesome udisks2 udiskie ntfs-3g
+Packages =(
+    "gimp "
+    "krita "
+    "inkscape "
+    "libreoffice-fresh"
+)
 
-cd ..
-cp -rf dotfiles/config/* .config
-cd dotfiles/theme
+pacman -Sy $Packages
 
-cp -rf Sweet-Dark-v40 /usr/share/themes
 
-echo "gtk-icon-theme-name = \"Adwaita\"                                                                              
-    gtk-theme-name = \"Sweet-Dark-v40\"
-    gtk-font-name = \"Cantarell 11\"">/usr/share/gtk-2.0/gtkrc
-
-echo "[Settings]
-gtk-icon-theme-name = Adwaita
-gtk-theme-name = Sweet-Dark-v40
-gtk-font-name = Cantarell 11">/usr/share/gtk-3.0/settings.ini
-
-rm -rf Sweet-Dark-v40
-cd ~
-rm -rf dotfiles
-
-//yay
-//fish defualt shell
 //vs code yay
+//onedrive
--- install.sh
+++ install.sh
@@ -1,20 +1,70 @@
-pacman -Sy firefox thunar ranger git neofetch picom neovim lightdm-webkit2-greeter fish dmenu rofi python python-psutil python-iwlib alsa-utils ttf-ubuntu-font-family ttf-font-awesome udisks2 udiskie ntfs-3g
+Packages =(
+    "firefox "
+    "qtile "
+    "lightdm "
+    "thunar "
+    "ranger "
+    "git "
+    "neofetch "
+    "picom "
+    "neovim "
+    "lightdm-webkit2-greeter "
+    "fish "
+    "dmenu "
+    "rofi "
+    "python "
+    "python-psutil "
+    "python-iwlib "
+    "alsa-utils "
+    "ttf-ubuntu-font-family "
+    "ttf-font-awesome "
+    "noto-fonts-emoji "
+    "udisks2 "
+    "udiskie "
+    "ntfs-3g " 
+    "pavucontrol "
+    "networkmanager "
+    "eog "
+    "mpv "
+)
 
-cd ..
-cp -rf dotfiles/config/* .config
-cd dotfiles/theme
+pacman -Sy $Packages
 
-cp -rf Sweet-Dark-v40 /usr/share/themes
+#yay
+pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
 
+#fish
+echo /usr/bin/fish | sudo tee -a /etc/shells
+chsh -s /usr/bin/fish
+
+
+curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+fish
+omf install slacker
+
+#config
+
+cp -rf config/* .config
+cp -rf faillock.conf /etc/security/faillock.conf
+
+#theme
+cp -rf theme/Sweet-Dark-v40 /usr/share/themes
+mkdir /usr/share/gtk-2.0
+touch /usr/share/gtk-2.0/gtkrc
 echo "gtk-icon-theme-name = \"Adwaita\"                                                                              
     gtk-theme-name = \"Sweet-Dark-v40\"
     gtk-font-name = \"Cantarell 11\"">/usr/share/gtk-2.0/gtkrc
 
+mkdir /usr/share/gtk-3.0
+touch /usr/share/gtk-3.0/settings.ini
 echo "[Settings]
 gtk-icon-theme-name = Adwaita
 gtk-theme-name = Sweet-Dark-v40
 gtk-font-name = Cantarell 11">/usr/share/gtk-3.0/settings.ini
 
-rm -rf Sweet-Dark-v40
-cd ~
-rm -rf dotfiles
+#service
+systenctl enable NetworkManager.service
+systemctl enable lightdm.service
+
+
+
